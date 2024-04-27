bba65065c5a98c3ffa648d35ede0796e2e88d1c8
rinbk201 <yamarin8zoo@gmail.com>
2024-04-27 20:28:34 +0900

    fix DeepSleepの動作確認完了

--- src/main.cpp
+++ src/main.cpp
@@ -1,220 +1,64 @@
 #include <Arduino.h>
 #include <SPI.h>
+#include "driver/gpio.h"
 // Need this for the lower level access to set them up.
-#include <HardwareSerial.h>
-const int ledPin = 4; // Xiao C3のGPIO4ピンを使用
+#include "esp_bt_main.h"
+#include "esp_bt.h"
+#include "esp_wifi.h"
+const int ledPin = 2; // Xiao C3のGPIO4ピンを使用
+RTC_DATA_ATTR int counter = 0;  //RTC coprocessor領域に変数を宣言することでスリープ復帰後も値が保持できる
 
-
-// Define two Serial devices mapped to the two internal UARTs
-HardwareSerial MySerial0(0);
-HardwareSerial MySerial1(1);
-
-int PORTLATE = 57600;
-int BIGTIMEOUT = 10000;
-int POSTTIMEOUT = 60000;
-int NORMALTIMEOUT = 5000;
-int SMALLTIMEOUT = 1000;
-
-unsigned char data[4] = {};
-
-int count = 0;
-float distance = -1;
-
-bool sendATCommand(const char* command , const int timeout) {
-  MySerial0.write(command);
-  MySerial0.flush();
-  delay(5000);  // 応答を待つための適切な遅延を設定
-
-  while (MySerial0.available()) {
-    String response = MySerial0.readStringUntil('\n');
-    Serial.println(response);
-
-    // エラーチェック
-    if  (response.indexOf("ERROR") != -1)  {
-      return false;  // エラーが検出された場合
-    }
-  }
-
-  return true;  
-
+void esp32c3_deepsleep(uint8_t sleep_time, uint8_t wakeup_gpio) {
+  // スリープ前にwifiとBTを明示的に止めないとエラーになる
+  esp_bluedroid_disable();
+  esp_bt_controller_disable();
+  esp_wifi_stop();
+  esp_deep_sleep_enable_gpio_wakeup(BIT(wakeup_gpio), ESP_GPIO_WAKEUP_GPIO_HIGH);  // 設定したIOピンがHIGHになったら目覚める
+  esp_deep_sleep(1000 * 1000 * sleep_time);
 }
 
-bool sendBody(const char* command ) {
-  MySerial0.write("AT+SHBOD=1024,10000\r\n"); 
-  MySerial0.flush();
-  delay(1000);
-  MySerial0.write(command);
-  MySerial0.flush();
-  String response = MySerial0.readStringUntil('\n');
-  Serial.println(response);
-  response += MySerial0.readStringUntil('\n');
-  String temp;
-  do {
-    temp = MySerial0.readStringUntil('\n');
+void wakeup_cause_print() {
+  esp_sleep_wakeup_cause_t wakeup_reason;
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+  switch (wakeup_reason) {
+    case 0: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_UNDEFINED"); break;
+    case 1: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_ALL"); break;
+    case 2: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_EXT0"); break;
+    case 3: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_EXT1"); break;
+    case 4: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_TIMER"); break;
+    case 5: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_TOUCHPAD"); break;
+    case 6: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_ULP"); break;
+    case 7: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_GPIO"); break;
+    case 8: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_UART"); break;
+    case 9: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_WIFI"); break;
+    case 10: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_COCPU"); break;
+    case 11: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_COCPU_TRAP_TRIG"); break;
+    case 12: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_BT"); break;
+    default: Serial.println("Wakeup was not caused by deep sleep"); break;
+  }
+}
+
+void setup() {
+  pinMode(ledPin, OUTPUT); // ピンを出力として設定
+  digitalWrite(ledPin, HIGH);
+  Serial.begin(57600);
+  Serial.printf("Cause:%d\r\n", esp_sleep_get_wakeup_cause());
+  wakeup_cause_print();
+  //Serial.printf("Counter:%d\r\n", counter++);
+  Serial.println("Start!!");
+  for (int i = 0; i < 15; i++) {
+    Serial.printf("%d analog :%d\n", i, analogRead(0));
+    Serial.printf("Counter:%d\r\n", counter++);
     delay(1000);
-    response += temp;
-    
-  } while (temp == "OK"||temp == "ERROR" || temp == "");
-  delay(3000);
-
-// エラーチェックとテキスト形式のレスポンスの出力
-if (response.indexOf("ERROR") != -1) {
-  Serial.println("Error in response");
-  return false;
-} else {
-  Serial.println(response);
-  Serial.flush();
-  return true;
+  }
+  Serial.println("Go to DeepSleep!!");
+  digitalWrite(ledPin, LOW); // LEDをOFFにする
+  esp32c3_deepsleep(10, 2);  //10秒間スリープ スリープ中にGPIO2がHIGHになったら目覚める
 }
 
-}
-
-
-void serial_send(float distance) {
-
-
-  if (!sendATCommand("AT+CFUN=6\r\n",NORMALTIMEOUT)) {
-    Serial.println("Error: AT+CFUN=6");
-    return;
-  }
-  delay(BIGTIMEOUT);
-
-  if (!sendATCommand("AT+CGDCONT=1,\"IP\",\"soracom.io\"\r\n",NORMALTIMEOUT)) {
-    Serial.println("Error: AT+CGDCONT=1");
-    return;
-  }
-  delay(SMALLTIMEOUT);
-   //for (int i = 0; i <= 5; i++) {
-    if (!sendATCommand("AT+CNACT=0,1\r\n",BIGTIMEOUT)) {
-      Serial.println("Error: AT+CNACT");
-      delay(1000);
-      
-      return;
-  }
-  delay(BIGTIMEOUT);
-
-  if (!sendATCommand("AT+SHCONF=\"URL\",\"http://harvest.soracom.io\"\r\n",NORMALTIMEOUT)) {
-    Serial.println("Error: AT+SHCONF URL");
-    return;
-  }
-  delay(SMALLTIMEOUT);
-
-
-
-
-  if (!sendATCommand("AT+SHCONF=\"BODYLEN\",1024\r\n",NORMALTIMEOUT)) {
-    Serial.println("Error: AT+SHCONF BODYLEN");
-    return;
-  }
-  delay(SMALLTIMEOUT);
-
-  if (!sendATCommand("AT+SHCONF=\"HEADERLEN\",350\r\n",NORMALTIMEOUT)) {
-    Serial.println("Error: AT+SHCONF HEADERLEN");
-    return;
-  }
-  delay(SMALLTIMEOUT);
-
-  if (!sendATCommand("AT+SHCONN\r\n",NORMALTIMEOUT)) {
-    Serial.println("Error: AT+SHCONN");
-    return;
-  }
-  delay(SMALLTIMEOUT);
-
-  if (!sendATCommand("AT+SHAHEAD=\"Content-Type\",\"application/json\"\r\n",NORMALTIMEOUT)) {
-    Serial.println("Error: AT+SHAHEAD");
-    return;
-  }
-  delay(SMALLTIMEOUT);
-
-  
-
-  String distance_json = "\"distance\":" + String(distance);
-  String All_data = "{" + distance_json +"}\r\n";
-
-
-  if (!sendBody(All_data.c_str())) {
-    Serial.println("Error: JSON Data");
-    return;
-  }
-  delay(SMALLTIMEOUT);
-
-  if (!sendATCommand("AT+SHREQ=\"http://harvest.soracom.io\",3\r\n",POSTTIMEOUT)) {
-    Serial.println("Error: AT+SHREQ");
-    return;
-  }
-  delay(NORMALTIMEOUT);
-
-  if (!sendATCommand("AT+SHDISC\r\n",NORMALTIMEOUT)) {
-    Serial.println("Error: AT+SHDISC");
-    return;
-  }
-
-  Serial.println("done");
+void loop() {
+  // put your main code here, to run repeatedly:
 }
 
 
 
-void setup()
-{
-    Serial.begin(PORTLATE);
-    // Configure MySerial0 on pins TX=6 and RX=7 (-1, -1 means use the default)
-    MySerial0.begin(PORTLATE, SERIAL_8N1, -1, -1);
-    MySerial1.begin(9600, SERIAL_8N1, 9, 10);
-    pinMode(ledPin, OUTPUT); // ピンを出力として設定
-    digitalWrite(ledPin, HIGH); // センサ類電源をONにする
-    Serial.println("ON");
-    count = 0;
-}
-
-void loop()
-{
-    do
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            data[i] = MySerial1.read();
-        }
-    } while (MySerial1.read() == 0xff);
-
-    MySerial1.flush();
-
-    if (data[0] == 0xff)
-    {
-        int sum;
-        sum = (data[0] + data[1] + data[2]) & 0x00FF;
-        if (sum == data[3])
-        {
-            distance = (data[1] << 8) + data[2];
-            if (distance > 30)
-            {
-                Serial.print("distance=");
-                Serial.print(distance / 10);
-                Serial.println("cm");
-            }
-            else
-            {
-                Serial.println("Below the lower limit");
-            }
-        }
-        else
-            Serial.println("ERROR");
-    }
-    delay(100);
-    count += 1;
-
-    if (count > 200 || distance != -1)//デバッグで＆から変更
-    {
-    delay(5000);
-    Serial.println("start");
-    delay(1000);
-    Serial.println("stand by");
-    serial_send(distance/10);
-    count = 0;
-    digitalWrite(ledPin, LOW); // センサ類電源をOFFにする
-    Serial.println("OFF");
-    delay(15000);
-    digitalWrite(ledPin, HIGH); // センサ類電源をONにする
-    Serial.println("ON");
-    distance = -1;
-    }
-}
